7230e9d829bee92dc34f9e56aeeac35ac8631f6e
ppmasa8 <kiyokiyopip56@gmail.com>
2021-07-23 02:57:58 +0900

    practice

---
+++ sort/insertSort.rb
@@ -0,0 +1,18 @@
+class Array
+    def insert_sort
+        len = self.size
+        (1...len).each do |i|
+            tmp = self[i]
+            j = i - 1
+            while j >= 0 && self[j] > tmp
+                self[j+1] = self[j]
+                j -= 1
+            end
+            self[j+1] = tmp
+            p self
+        end
+    end
+end
+
+array = Array.new(10){rand(10)}
+array.insert_sort
