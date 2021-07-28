a3d4d580140bd4f81c37632e3e13fbae3c262295
ppmasa8 <kiyokiyopip56@gmail.com>
2021-07-28 16:14:38 +0900

    comp

--- rasenbon/data-structure/linkedlist.rb
+++ rasenbon/data-structure/linkedlist.rb
@@ -28,7 +28,7 @@ class List
     end
     private :search
 
-    def inseart!(n, data)
+    def insert!(n, data)
         cp = search(n - 1)
         if cp
             cp.link = Cell.new(data, cp.link)
@@ -54,6 +54,63 @@ class List
         !@top.link
     end
 
+    def [](n)
+        at(n)
+    end
+
+    def []=(n, value
+        cp = search(n)
+    if cp
+      cp.data = value
+    end
+  end
+  
+  def append(cp1, cp2)
+    if !cp1
+      cp2
+    else
+      Cell.new(cp1.data, append(cp1.link, cp2))
+    end
+  end
+  private :append
+  
+  def +(other)
+    raise TypeError unless other.instance_of?(List)
+    ls = List.new
+    ls.top.link = append(@top.link, other.top.link)
+    ls
+  end
+  
+  def *(n)
+    ls = List.new
+    n.times do
+      ls.top.link = append(@top.link, ls.top.link)
+    end
+    ls
+  end
+  
+  def each
+    cp = @top.link
+    while cp
+      yield cp.data
+      cp = cp.link
+    end
+    self
+  end
+  
+  def to_a
+    a = []
+    each do |x| a.push(x) end
+    a
+  end
+  
+  def to_s
+    "(" << to_a.join(",") << ")"
+  end
+  
+  def inspect
+    sprintf("#<List:%#x>", self.object_id)
+  end
 
 
 end
